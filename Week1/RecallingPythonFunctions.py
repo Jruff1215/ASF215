3061903b13caa134a067cd8e4b3d26174f03c29f
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2022-05-24 15:13:15 -0400

    update

--- Week1/RecallingPythonFunctions.py
+++ Week1/RecallingPythonFunctions.py
@@ -11,10 +11,12 @@ class List():
         self.list.append(newDict)
     def addTuple(self, *item):
         self.list.append(item)   
-
+    def tupleList(self):
+        self.list = tuple(self.list)
 itemList = List()
 itemList.addItem(22)
 itemList.addItem("Hello World")
 itemList.addDict({"type": "Dog"},{ "breed": "Dachshund"}, {"color": "Black/Brown"})
 itemList.addTuple("Doxen", "Weenie", "Weiner Dog")
+itemList.tupleList()
 print(itemList.list)
